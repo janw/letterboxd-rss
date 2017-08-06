163883d18695bba432d915bdc797e9856a9fcd63
Jan Willhaus <mail@janwillhaus.de>
2017-08-06 11:43:30 +0200

    Iterate over pages, fill feed completely

--- watchlist2rss.py
+++ watchlist2rss.py
@@ -17,51 +17,58 @@ def main():
     base_url = 'https://letterboxd.com/'
     page_title = 'The Dude\'s Watchlist'
 
-    s = session()
-    r = s.get(watchlist_url)
-
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    watchlist_title = soup.find('meta', attrs={'property': 'og:title'})
-    if watchlist_title is not None:
-        page_title = watchlist_title.attrs['content']
-
     feed = FeedGenerator()
     feed.title(page_title)
     feed.id(watchlist_url)
     feed.link(href=watchlist_url, rel='alternate')
     feed.description(page_title + ' from Letterboxd')
 
-    posters = soup.findAll('div', attrs={'class', 'poster'})
-    match_imdb = re.compile('^http://www.imdb.com')
+    s = session()
+    page = 0
+    total_movies = 0
+    while True:
+        page += 1
+        r = s.get(watchlist_url + '/page/%i/' % page)
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    if len(posters) == 0:
-        return
-    elif len(posters) > feedlen:
-        posters = posters[:feedlen]
+        if page == 1:
+            watchlist_title = soup.find('meta', attrs={'property': 'og:title'})
+            if watchlist_title is not None:
+                page_title = watchlist_title.attrs['content']
 
-    print('Adding %i movies ...' % len(posters))
+        posters = soup.findAll('div', attrs={'class', 'poster'})
+        match_imdb = re.compile('^http://www.imdb.com')
 
-    for movie in posters:
+        if len(posters) == 0:
+            print('No more movies on page %i' % (page, ))
+            break
+        elif total_movies + len(posters) > feedlen:
+            posters = posters[:feedlen-total_movies]
 
-        movie_page = s.get(base_url + movie.attrs['data-film-slug'])
-        movie_soup = BeautifulSoup(movie_page.text, 'html.parser')
+        print('Adding %i movies from page %i' % (len(posters), page))
+        total_movies += len(posters)
 
-        movie_title = movie_soup.find('meta', attrs={'property': 'og:title'}).attrs['content']
-        movie_link = movie_soup.find('a', attrs={'href': match_imdb}).attrs['href']
-        movie_link = movie_link[:-11]
-        movie_description = movie_soup.find('div', attrs={'class': 'truncate'})
-        movie_description = movie_description.text.strip()
+        for movie in posters:
 
-        item = feed.add_item()
-        item.title(movie_title)
-        item.description(movie_description)
-        item.link(href=movie_link, rel='alternate')
-        item.guid(movie_link)
+            movie_page = s.get(base_url + movie.attrs['data-film-slug'])
+            movie_soup = BeautifulSoup(movie_page.text, 'html.parser')
 
-        print('Added movie', movie_title)
+            movie_title = movie_soup.find('meta', attrs={'property': 'og:title'}).attrs['content']
+            movie_link = movie_soup.find('a', attrs={'href': match_imdb}).attrs['href']
+            movie_link = movie_link[:-11]
+            movie_description = movie_soup.find('div', attrs={'class': 'truncate'})
+            movie_description = movie_description.text.strip()
 
-    feed.rss_file(output_file)
+            item = feed.add_item()
+            item.title(movie_title)
+            item.description(movie_description)
+            item.link(href=movie_link, rel='alternate')
+            item.guid(movie_link)
+
+            print(movie_title)
+
+    if total_movies > 0:
+        feed.rss_file(output_file)
 
 
 if __name__ == '__main__':
