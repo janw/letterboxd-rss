35ec7a29713034fdb39f2b9673a849f1c0d9e2cd
Jan Willhaus <mail@janwillhaus.de>
2019-04-24 19:53:59 +0200

    Last state of work on the simple script version

--- letterboxd_rss/__init__.py
+++ letterboxd_rss/__init__.py
@@ -5,72 +5,86 @@ from configparser import ConfigParser
 from requests import session
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
-match_imdb = re.compile('^http://www.imdb.com')
 
+match_imdb = re.compile("^http://www.imdb.com")
+base_url = "https://letterboxd.com/"
+
+MATCH_TOTAL_MOVIES = re.compile("to see (\d+)")
 ROOT_PATH = path.dirname(path.realpath(__file__))
 c = ConfigParser()
 c.read(path.join(ROOT_PATH, "config.ini"))
+s = session()
 
 
 def main():
-    feedlen = c['default'].getint('feed_length', 100)
-    watchlist_url = c['default'].get('watchlist_url', 'https://letterboxd.com/janwh/watchlist/')
-    output_file = c['default'].get('output_file', 'feed.xml')
-    base_url = 'https://letterboxd.com/'
-    page_title = 'The Dude\'s Watchlist'
+    feedlen = c["default"].getint("feed_length", 20)
+    watchlist_url = c["default"].get(
+        "watchlist_url", "https://letterboxd.com/janwh/watchlist/"
+    )
+    output_file = c["default"].get("output_file", "feed.xml")
+    page_title = "The Dude's Watchlist"
 
     feed = FeedGenerator()
     feed.title(page_title)
     feed.id(watchlist_url)
-    feed.link(href=watchlist_url, rel='alternate')
-    feed.description(page_title + ' from Letterboxd')
+    feed.link(href=watchlist_url, rel="alternate")
+    feed.description(page_title + " from Letterboxd")
 
-    s = session()
-    page = 0
-    total_movies = 0
-    while True:
-        page += 1
-        r = s.get(watchlist_url + '/page/%i/' % page)
-        soup = BeautifulSoup(r.text, 'html.parser')
+    # Get first page, gather general data
+    r = s.get(watchlist_url)
+    soup = BeautifulSoup(r.text, "html.parser")
 
-        if page == 1:
-            watchlist_title = soup.find('meta', attrs={'property': 'og:title'})
-            if watchlist_title is not None:
-                page_title = watchlist_title.attrs['content']
+    watchlist_title = soup.find("meta", attrs={"property": "og:title"})
+    page_title = watchlist_title.attrs["content"]
 
-        posters = soup.findAll('div', attrs={'class', 'poster'})
+    m = soup.find(text=MATCH_TOTAL_MOVIES)
+    if len(m) > 0:
+        total_movies = MATCH_TOTAL_MOVIES.search(m).group(1)
+        total_movies = int(total_movies)
 
-        if len(posters) == 0:
-            print('No more movies on page %i' % (page, ))
-            break
-        elif total_movies + len(posters) > feedlen:
-            posters = posters[:feedlen-total_movies]
+    last_page = soup.find_all("li", attrs={"class": "paginate-page"})[-1].text
+    last_page = int(last_page)
 
-        print('Adding %i movies from page %i' % (len(posters), page))
-        total_movies += len(posters)
+    for page in range(1, last_page):
+        if page > 1:
+            r = s.get(watchlist_url + "/page/%i/" % page)
+            soup = BeautifulSoup(r.text, "html.parser")
+            print()
 
-        for movie in posters:
+        ul = soup.find("ul", attrs={"class": "poster-list"})
+        movies = ul.find_all("li")
+        movies_on_page = len(movies)
 
-            movie_page = s.get(base_url + movie.attrs['data-film-slug'])
-            movie_soup = BeautifulSoup(movie_page.text, 'html.parser')
-            try:
-                movie_title = movie_soup.find('meta', attrs={'property': 'og:title'}).attrs['content']
-                movie_link = movie_soup.find('a', attrs={'href': match_imdb}).attrs['href']
-                movie_link = movie_link[:-11]
-            except AttributeError:
-                print('Parsing failed')
-            movie_description = movie_soup.find('div', attrs={'class': 'truncate'})
-            movie_description = movie_description.text.strip()
+        print("Gathering on page %i (%i movies)" % (page, movies_on_page))
+        print()
 
-            item = feed.add_item()
-            item.title(movie_title)
-            item.description(movie_description)
-            item.link(href=movie_link, rel='alternate')
-            item.guid(movie_link)
-
-            print(movie_title)
+        for movie in movies:
+            extract_metadata(movie, feed)
 
     if total_movies > 0:
         feed.rss_file(output_file)
 
 
+def extract_metadata(movie, feed):
+    movie_slug = movie.div.attrs["data-film-slug"]
+    movie_page = s.get(base_url + movie_slug)
+    movie_soup = BeautifulSoup(movie_page.text, "html.parser")
+
+    try:
+        movie_title = movie_soup.find("meta", attrs={"property": "og:title"}).attrs[
+            "content"
+        ]
+        movie_link = movie_soup.find("a", attrs={"href": match_imdb}).attrs["href"]
+        movie_link = movie_link[:-11]
+        movie_description = movie_soup.find("div", attrs={"class": "truncate"})
+        if movie_description is not None:
+            movie_description = movie_description.text.strip()
+        print(movie_title)
+    except AttributeError:
+        print("Parsing failed on", base_url + movie_slug)
+
+    item = feed.add_item()
+    item.title(movie_title)
+    item.description(movie_description)
+    item.link(href=movie_link, rel="alternate")
+    item.guid(movie_link)
